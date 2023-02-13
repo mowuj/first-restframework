9f1e5a5f61da796204ca2da484f6922539e9c66b
mowuj <mowuj92gmail.com>
2023-02-13 17:08:58 +0600

    update retrieve api

--- firstapp/urls.py
+++ firstapp/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('registration/',registrationAPI),
     path('contact/', ContactAPIViewOne.as_view()),
     path('post/', PostCreateAPIView.as_view()),
-    path('postlist/', PostListAPIView.as_view()),
+    # path('post/<int:id>/', PostRetrieveAPIView.as_view()),
+    path('post/<int:id>/', PostUpdateAPIView.as_view()),
+    # path('postlist/', PostListAPIView.as_view()),
 ]
--- firstapp/views.py
+++ firstapp/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 from rest_framework.permissions import IsAuthenticated,AllowAny
 from .models import *
 from rest_framework.views import APIView
-from rest_framework.generics import CreateAPIView,ListAPIView,ListCreateAPIView
+from rest_framework.generics import CreateAPIView,ListAPIView,ListCreateAPIView,RetrieveAPIView,UpdateAPIView,RetrieveUpdateAPIView
 
 def homeView(request):
     return render(request,'index.html')
@@ -125,7 +125,8 @@ class ContactAPIViewOne(APIView):
         return Response(serializer.data)
 
 from rest_framework import status
-class PostCreateAPIView(CreateAPIView):
+# List create view 
+class PostCreateAPIView(ListCreateAPIView):
     permission_classes=[IsAuthenticated,]
     queryset=BlogPost.objects.all()
     serializer_class=PostSerializer
@@ -139,9 +140,62 @@ class PostCreateAPIView(CreateAPIView):
         headers = self.get_success_headers(serializer.data)
         serializer=PostDetailSerializer(instance=instance,many=False)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
-    
-class PostListAPIView(ListAPIView):
-    permission_classes=[IsAuthenticated]
-    queryset=BlogPost.objects.all()
-    serializer_class = PostDetailSerializer
+        # List cerate api 
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
 
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = PostDetailSerializer(queryset, many=True)
+        return Response(serializer.data)
+    # Query Set customize
+    def get_queryset(self):
+        queryset=BlogPost.objects.filter(is_active=True)
+        return queryset
+    # List API VIEW 
+# class PostListAPIView(ListAPIView):
+#     permission_classes=[IsAuthenticated]
+#     queryset=BlogPost.objects.all()
+#     serializer_class = PostDetailSerializer
+
+class PostRetrieveAPIView(RetrieveAPIView):
+    permission_classes=[IsAuthenticated]
+    queryset = BlogPost.objects.all()
+    serializer_class = PostSerializer
+    lookup_field='id'
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = PostDetailSerializer(instance)
+        return Response(serializer.data)
+
+
+class PostUpdateAPIView(RetrieveUpdateAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = BlogPost.objects.filter(is_active=True)
+    serializer_class = PostSerializer
+    lookup_field='id'
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = PostDetailSerializer(instance)
+        return Response(serializer.data)
+    
+    def perform_update(self, serializer):
+        return serializer.save(user=self.request.user)
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        instance= self.perform_update(serializer)
+        serializer = PostDetailSerializer(instance)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+
+        return Response(serializer.data)
